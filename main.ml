7f0159231000c0251bee9395171671aa06ed3284
Denis Korzunov <disya2@gmail.com>
2019-01-07 22:49:07 +1100

    The main executable now parses the stdin

--- main.ml
+++ main.ml
@@ -1,22 +1,20 @@
-open Nix.Lexer
+open Nix
 
 let main () =
   let lexbuf = stdin
                |> Lexing.from_channel
-               |> (set_filename "<stdin>")
+               |> (Lexer.set_filename "<stdin>")
   in
   let q = Queue.create () in
   let s = ref [] in
-  let rec loop = function
-    | EOF ->  ()
-    | token ->
-      Printf.printf "%s\n" (print_token token);
-      loop (next_token q s lexbuf)
-  in
   try
-    loop (next_token q s lexbuf);
+    let parse_tree = Parser.main (Lexer.next_token q s) lexbuf in
+    SimplePrinter.print stdout parse_tree;
+    print_newline ()
   with
-    Error msg ->
-    Printf.eprintf "%s\n" msg
+  | Lexer.Error msg ->
+    Printf.eprintf "lexing error: %s\n" msg
+  | Parser.Error ->
+    Printf.eprintf "parse error at: %s\n" (Lexer.print_position lexbuf)
 
 let () = main ()
