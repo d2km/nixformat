e34a21b509cf9934f4712974f3725be9e5557e32
Denis Korzunov <disya2@gmail.com>
2019-01-08 22:41:28 +1100

    Accept file names as input parameters

--- main.ml
+++ main.ml
@@ -1,20 +1,27 @@
 open Nix
 
+let file_names = ref ([]: string list)
+let out = ref stdout
+
 let main () =
-  let lexbuf = stdin
-               |> Lexing.from_channel
-               |> (Lexer.set_filename "<stdin>")
+  Arg.parse [] (fun x -> file_names := x :: !file_names) "";
+  let files = match !file_names with
+    | [] -> [stdin, "<stding>"]
+    | names -> List.map (fun n -> open_in n, n) names
   in
-  let q = Queue.create () in
-  let s = ref [] in
-  try
-    let parse_tree = Parser.main (Lexer.next_token q s) lexbuf in
-    SimplePrinter.print stdout parse_tree;
-    print_newline ()
-  with
-  | Lexer.Error msg ->
-    Printf.eprintf "lexing error: %s\n" msg
-  | Parser.Error ->
-    Printf.eprintf "parse error at: %s\n" (Lexer.print_position lexbuf)
+  List.iter (fun (file, name) ->
+      let lexbuf = Lexer.set_filename name (Lexing.from_channel file) in
+      let q, s = Queue.create (), ref [] in
+      try
+        lexbuf
+        |> Parser.main (Lexer.next_token q s)
+        |> SimplePrinter.print !out;
+        output_char !out '\n'
+      with
+      | Lexer.Error msg ->
+        Printf.eprintf "lexing error: %s\n" msg
+      | Parser.Error ->
+        Printf.eprintf "parse error at: %s\n" (Lexer.print_position lexbuf)
+    ) files
 
 let () = main ()
